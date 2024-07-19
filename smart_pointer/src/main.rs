f6702c5e268a6db9b3d131530fd5290467751a1e
Bavenraj <bavenraj08@gmail.com>
2024-07-19 14:54:49 +0800

    smart_pointer

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
@@ -10,17 +12,46 @@ enum Message {
     Write(String),
     ChangeColor(i32,i32,i32),
 }
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T>{
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 fn main() {
     use crate::List::{Cons, Nil};
     let b = Box::new(5);
+    
     // box type allow ownership transfer but does implement copy trait even if it was i32
-    //let a = b;
+    let a = *b.deref();
     println!("b = {b}");
-    //println!("a = {a}");
+    println!("a = {a}");
 
     let list = Box::new(Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil)))))));
-    //let list2 = Cons(1, Cons(2, Cons(3, Nil)));
-
     println!("{list:?}");
-    //println!("{list2:?}");
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    //MyBox type has no deref trait
+    assert_eq!(5, *y);
+    println!("x is {x} and y is {}", *(y));
+
+    //deref method return reference and need to be dereference
+    assert_eq!(*(y.deref()), *y.deref());
+
+    let a1 = 6;
+    let a2 = &a1;
+    //let a3 = *a2.deref();
+    println!("{:p}, {}, {}", a2, a1, a2.deref());
 }
