7221e76bc384b36d1642dce1a83f3e279e80633a
Bavenraj <bavenraj08@gmail.com>
2024-07-19 11:40:30 +0800

    smart_pointer

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -4,10 +4,19 @@ enum List {
     Nil,
 }
 
+enum Message {
+    Quit,
+    Move { x:i32, y:i32},
+    Write(String),
+    ChangeColor(i32,i32,i32),
+}
 fn main() {
     use crate::List::{Cons, Nil};
     let b = Box::new(5);
+    // box type allow ownership transfer but does implement copy trait even if it was i32
+    //let a = b;
     println!("b = {b}");
+    //println!("a = {a}");
 
     let list = Box::new(Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil)))))));
     //let list2 = Cons(1, Cons(2, Cons(3, Nil)));
