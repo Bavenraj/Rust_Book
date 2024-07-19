9c6d040a9671fc0c4c26b934f5997411bbf8ee05
Bavenraj <bavenraj08@gmail.com>
2024-07-19 11:26:58 +0800

    smart_pointer

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -1,12 +1,17 @@
+#[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
 }
 
 fn main() {
-   
+    use crate::List::{Cons, Nil};
     let b = Box::new(5);
     println!("b = {b}");
 
-    let list = Cons(1, Cons(2, Cons(3, Nil)));
+    let list = Box::new(Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil)))))));
+    //let list2 = Cons(1, Cons(2, Cons(3, Nil)));
+
+    println!("{list:?}");
+    //println!("{list2:?}");
 }
